6e3e8b3e04b8898e3c07c66e09347fa3c16b4268
Виталий Вит <vitalik.vit37@gmail.com>
2019-09-20 18:21:34 +0300

    s

--- DZlevelUP_1309/Program.cs
+++ DZlevelUP_1309/Program.cs
@@ -8,31 +8,31 @@ namespace DZlevelUP_1309
     {
         static void Main(string[] args)
         {
-            Factorial();
+            Print();
 
 
 
 
 
             Console.ReadKey();
+        }
 
-
+        static int TryParseNums()
+        {
+            int i;
+            while (!int.TryParse(Console.ReadLine(), out i))
+            {
+                Console.WriteLine("Введите число!");
+            }
+            return i;
         }
 
         static void Factorial()
         {
             Console.WriteLine("Диапазон вычисления Факториала: 0 - 170!");
             Console.Write("Введите число: ");
-            string s = Console.ReadLine();
-
-            double nums;
-
-
-            while (!double.TryParse(s, out nums))
-            {
-                Console.WriteLine("Введите число!");
-                s = Console.ReadLine();
-            }
+            
+            double nums = TryParseNums();
 
             double res = GetFactorial(nums);
 
@@ -46,12 +46,12 @@ namespace DZlevelUP_1309
             }
 
         }
+
         static double GetFactorial(double x)
         {
-
             if (x >= 0 && x <= 170)
             {
-                if (x == 0)
+                if (x == 1 || x == 0)
                 {
                     return 1;
                 }
@@ -59,19 +59,87 @@ namespace DZlevelUP_1309
                 {
                     return x * GetFactorial(x - 1);
                 }
-
             }
             else
             {
                 return -1;
             }
-           
         }
 
-        
+        static void ComparisonOfNumbers()
+        {
+            Console.WriteLine("Сколько чисел хотите сравнить?");
+
+            int AmountNumbers = TryParseNums();
+            int[] arrayNums = new int[AmountNumbers];
+
+            for (int i = 0; i < arrayNums.Length; i++)
+            {
+                Console.Write("{0}-е число: ", i + 1);
+                arrayNums[i] = TryParseNums();
+            }
+
+            int count = arrayNums.Length,
+            number = arrayNums[arrayNums.Length - 1];
+
+            Console.WriteLine("Min: " + GetFindingMinNumber(arrayNums, count, number));
+            Console.WriteLine("Max: " + GetFindingMaxNumber(arrayNums, count, number));
+        }
+
+        static int GetFindingMinNumber(int[] mass, int count, int number) //
+        {
+            int min = number;
+            if (count < 1)
+            {
+                return min;
+            }
+
+            if (min > mass[count - 1])
+            {
+                min = mass[count - 1];
+            }
+
+            GetFindingMinNumber(mass, count - 1, min);
+            return GetFindingMinNumber(mass, count - 1, min); ;
+        }
+
+        static int GetFindingMaxNumber(int[] mass, int count, int number)
+        {
+            int max = number;
+            if (count < 1)
+            {
+                return max;
+            }
+
+            if (max < mass[count - 1])
+            {
+                max = mass[count - 1];
+            }
+
+            GetFindingMaxNumber(mass, count - 1, max);
+            return GetFindingMaxNumber(mass, count - 1, max); ;
+        }
+
+        static void CleaningMethod()
+        {
+            Console.WriteLine("Для продолжения нажмите любую кнопку (:");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void Print()
+        {
+            Console.WriteLine("Первое задание\nВычисление факториала");
+            Factorial();
+            CleaningMethod();
+
+            Console.WriteLine("Второе задание\nНахождение Min и Max чисел рекурсией");
+            ComparisonOfNumbers();
+            CleaningMethod();
 
 
 
-
+        }
+                       
     }
 }
